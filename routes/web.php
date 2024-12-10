37574d781b47877f0167497b159eb3ca87e73860
cristianlumertz <cristianlumertz@gmail.com>
2024-12-10 18:11:04 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminMainController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'rolemanager:costumer'])->name('dashboard');
+
+//admin routes
+Route::middleware(['auth', 'verified', 'rolemanager:admin'])->group(function () {
+    Route::controller(AdminMainController::class)->group(function () {
+        Route::prefix('admin')->group(function () {
+            Route::get('/dashboard', 'index')->name('admin');
+        });
+    });
+});
+
+Route::get('/vendor/dashboard', function () {
+    return view('vendor');
+})->middleware(['auth', 'verified', 'rolemanager:vendor'])->name('vendor');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
